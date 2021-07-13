f4882519f067a85eb9a63d110df83883784d2a01
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2021-07-13 14:20:29 -0700

    Fix alter field migration error

--- classify/migrations/0019_alter_frontendpackage_bin.py
+++ classify/migrations/0019_alter_frontendpackage_bin.py
@@ -10,7 +10,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.RemoveField(
+            model_name='frontendpackage',
+            name='bin',
+        ),
+        migrations.AddField(
             model_name='frontendpackage',
             name='bin',
             field=models.JSONField(),
