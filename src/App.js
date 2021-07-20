ac91873e64d717caed1db6e4fcbade32ef3b1594
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2021-07-20 16:48:00 -0700

    Fix register/login render bugs

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom"
+
 import Header from './components/layout/Header';
 import Annotations from './components/annotations/Annotations';
 import Login from './components/accounts/Login';
 import Register from './components/accounts/Register';
-import PrivateRoute from './common';
+import PrivateRoute from './components/common/PrivateRoute';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
     componentDidMount() {
-        store.dispatch(loadUser());
+        
     }
 
     render() {
         return(
-            <Router>
-            <body>
-            <Header />
-            <main>
-                <div class="page">
+            <Provider store={store}>
+                <Router>
+                    <body>
+                        <Header />
+                        <main>
+                            <div class="page">
 
-                <div class="content">
-                    <Switch>
-                    <PrivateRoute exact path="/" component={Annotations} />
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/login" component={Login} />
-                    </Switch>
-                </div>
+                            <div class="content">
+                                <Switch>
+                                    <PrivateRoute exact path="/" component={Annotations} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                </Switch>
+                            </div>
 
-                </div>
-            </main>
+                            </div>
+                        </main>
 
-            <footer>
-            </footer>
-
-            </body>
-            
-            </Router>
+                        <footer>
+                        </footer>
+                    </body>
+                </Router>
+            </Provider>
         );
     }
 
