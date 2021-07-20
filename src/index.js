58cc75adecfd245f5e9684caa67d78a189768b54
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2021-07-20 15:32:53 -0700

    Restructure Annotations as component

--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 import './css/master.css';
 import './css/classify-styles.css';
-import Header from './components/layout/Header';
-import Annotations from './App';
-import Login from './components/accounts/Login';
-import Register from './components/accounts/Register';
-import PrivateRoute from './common';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-
-    <Router>
-    <body>
-      <Header />
-      <main>
-        <div class="page">
-
-          <div class="content">
-            <Switch>
-              <PrivateRoute exact path="/" component={Annotations} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-            </Switch>
-          </div>
-
-        </div>
-      </main>
-
-      <footer>
-      </footer>
-
-    </body>
-      
-    </Router>
-
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
