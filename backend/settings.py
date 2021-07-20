f65e4989143ad10b0ad1dc6e2f4d57a9283542e7
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2021-07-20 13:04:11 -0700

    Fix knox authentication bug

--- backend/settings.py
+++ backend/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'knox.auth.TokenAuthentication',
+        ('knox.auth.TokenAuthentication',)
     ]
 }
 
