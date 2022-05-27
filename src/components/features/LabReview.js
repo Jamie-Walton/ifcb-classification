00442fc86c0c4ef0c289c020cee9842a48e1374e
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2022-05-27 14:44:05 -0700

    Add filtering to Lab Review

--- src/components/features/LabReview.js
+++ src/components/features/LabReview.js
@@ -33,8 +33,6 @@ class LabFilePreview extends Component {
             appearance = 'community-file-preview-categorized';
         }
 
-        // <p className="community-file-classifier">{this.props.classifier.length > 0 ? 'Completed by' + this.props.classifier : ''}</p>
-
         return (
             <div className={"community-file-preview-container " + appearance}>
                 <div>
@@ -55,13 +53,43 @@ class LabFilePreview extends Component {
     }
 }
 
+class Filter extends Component {
+    renderOption(option) {
+        return(
+            <div className="filter-option" onClick={() => this.props.applyFilter(this.props.filter, option)}>{option}</div>
+        );
+    }
+    
+    render() {
+        return(
+            <div>
+                <div className="filter-choice" onClick={() => this.props.onClick(this.props.filter)}>
+                    <p className="filter-choice-text">{this.props.filter}</p>
+                    <p className="filter-choice-plus" id={this.props.filter + "-plus"}>+</p>
+                </div>
+                <div className="filter-dropdown" id={this.props.filter + "-dropdown"}>
+                    {this.props.options.map((option) => (this.renderOption(option)))}
+                </div>
+            </div>
+        );
+    }
+}
+
 class LabReview extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            originalFiles: [],
             files: [],
             selectedTimeseries: '',
             selectedFile: '',
+            users: [],
+            days: [],
+            timeseries: [],
+            ifcbs: [],
+            statuses: ['Complete', 'Incomplete'],
+            appliedFilters: [],
+            rowCount: 0,
         }
     }
 
@@ -74,13 +102,70 @@ class LabReview extends Component {
         goto_communityfile: PropTypes.func,
     }
 
+    getDate(file) {
+        const timestamp = file.slice(1,5) + '-' + file.slice(5,7) + '-' + file.slice(7,12) + ':' + 
+            file.slice(12,14) + ':' + file.slice(14,16);
+        const date = new Date(timestamp);
+        const dateString = date.toDateString().slice(4,10) + ',' + date.toDateString().slice(10,);
+        
+        return dateString
+    }
+
+    filterFiles() {
+        var files = [];
+        for (let filter of this.state.appliedFilters) {
+            switch(filter.category) {
+                case 'user':
+                    files = files.concat(this.state.originalFiles.filter(f => f.completion_marker === filter.choice))
+                    break
+                case 'day':
+                    var date = new Date(filter.choice)
+                    var day = date.toISOString().slice(5,10)
+                    var year = date.toISOString().slice(0,4)
+                    files = files.concat(this.state.originalFiles.filter(f => f.day === day).filter(f => f.year === year))
+                    break
+                case 'timeseries':
+                    files = files.concat(this.state.originalFiles.filter(f => f.timeseries === filter.choice))
+                    break
+                case 'ifcb':
+                    files = files.concat(this.state.originalFiles.filter(f => f.ifcb === filter.choice))
+                    break
+                case 'status':
+                    var status = (filter.choice === 'Complete') ? true : false
+                    files = files.concat(this.state.originalFiles.filter(f => f.complete === status))
+                    break
+                default:
+                    break
+            }
+        }
+        files = [... new Set(files)]
+        files = files.sort((a, b) => (a.file < b.file) ? 1 : -1);
+        this.setState({ files: files });
+    }
+
     componentDidMount() {
         axios
             .get('/bins/')
             .then((res) => {
-                this.setState({ files: res.data.reverse() })
+                const files = res.data.sort((a, b) => (a.file < b.file) ? 1 : -1);
+                this.setState({ 
+                    files: files,
+                    originalFiles: files,
+                    users: [...new Set(files.map(f => f.completion_marker))].filter(n => n!==null),
+                    days: [...new Set(files.sort().map(f => this.getDate(f.file)))],
+                    timeseries: [...new Set(files.map(f => f.timeseries))],
+                    ifcbs: [...new Set(files.map(f => f.ifcb))],
+                    rowCount: Math.floor(files.length/4)+1,
+                })
+
             })
             .catch((err) => console.log(err));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.appliedFilters !== prevState.appliedFilters) {
+            this.filterFiles();
+        }
     }
 
     handleFileClick(timeseries, file) {
@@ -101,6 +186,46 @@ class LabReview extends Component {
                     <div className='legend-bubble incomplete-bubble' />
                     <p className='legend-text'>Incomplete</p>
                 </div>
+            </div>
+        );
+    }
+
+    handleFilterChoiceClick(option) {
+        document.getElementById(option + '-plus').classList.toggle('rotate-plus');
+        document.getElementById(option + '-dropdown').classList.toggle('show-filter-dropdown');
+    }
+
+    handleApplyFilter(category, choice) {
+        var appliedFilters = this.state.appliedFilters;
+        if(!appliedFilters.includes({category: category, choice: choice})) {
+            appliedFilters.push({category: category, choice: choice});
+        }
+        this.setState({ appliedFilters: appliedFilters });
+    }
+
+    handleRemoveFilter(filter) {
+        const applied = this.state.appliedFilters;
+        const index = applied.findIndex(entry => entry === filter);
+        const newAppliedFilters = applied.slice(0, index).concat(applied.slice(index+1,applied.length));
+        this.setState({ appliedFilters: newAppliedFilters });
+    }
+
+    renderFilterChoice(filter, options) {
+        return(
+            <Filter
+                filter={filter}
+                options={options}
+                onClick={(option) => this.handleFilterChoiceClick(option)}
+                applyFilter={(category, choice) => this.handleApplyFilter(category, choice)}
+            />
+        );
+    }
+
+    renderAppliedFilters(filter) {
+        return (
+            <div className="filter-choice applied" onClick={() => this.handleRemoveFilter(filter)}>
+                <p className="filter-choice-text applied-text">{filter.category.toUpperCase() + ': ' + filter.choice}</p>
+                <p className="filter-choice-plus applied-text rotate-plus" id={this.props.filter + "-plus"}>+</p>
             </div>
         );
     }
@@ -183,18 +308,27 @@ class LabReview extends Component {
                             </div>
                             <h2 className="analysis-option-heading page-heading community-review-heading">Lab Review</h2>
                             {this.renderLegend()}
+                            <div className="applied-filters applied-filter-labreview">{this.state.appliedFilters.map((filter) => (this.renderAppliedFilters(filter)))}</div>
+                            <div className="filter-buttons filter-buttons-labreview">
+                                {this.renderFilterChoice('user', this.state.users)}
+                                {this.renderFilterChoice('day', this.state.days)}
+                                {this.renderFilterChoice('timeseries', this.state.timeseries)}
+                                {this.renderFilterChoice('ifcb', this.state.ifcbs)}
+                                {this.renderFilterChoice('status', this.state.statuses)}
+                            </div>
                             <div>
-                                {this.state.files.length > 1 ?
+                                {this.state.files.length > 0 ?
                                     <Grid
                                         width={document.documentElement.clientWidth*0.8}
                                         height={800}
                                         columnWidth={document.documentElement.clientWidth*0.8/4}
                                         rowHeight={document.documentElement.clientWidth*0.11}
-                                        rowCount={Math.floor(this.state.files.length/4)+1}
+                                        rowCount={this.state.rowCount}
                                         columnCount={4}
                                         cellRenderer={cellRenderer}
                                         scrollToAlignment="start"
                                         className="community-review-grid"
+                                        files={this.state.files}
                                     /> :
                                     <div/>
                                 }
